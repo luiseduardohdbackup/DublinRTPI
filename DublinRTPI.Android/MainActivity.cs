dca8cf0c0319a471ed8e3086188eb30d5159ea82
Remo Jansen <rhjm.gnu@gmail.com>
2014-02-26 00:30:12 +0000

    Did some progress on Android app

--- DublinRTPI.Android/MainActivity.cs
+++ DublinRTPI.Android/MainActivity.cs
@@ -5,28 +5,51 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Maps;
+using DublinRTPI.Core.Entities;
 
 namespace DublinRTPI.Android
 {
-	[Activity (Label = "DublinRTPI.Android", MainLauncher = true)]
+	[Activity (Label = "Dublin RTPI", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		private MapHelper _mapHelper;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
-			// Set our view from the "main" layout resource
+			// Enable tab navigation
 			ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+
+			// Dsibale ActionBar Icon
+			ActionBar.SetHomeButtonEnabled (false);
+			//ActionBar.SetDisplayShowHomeEnabled (false);
+
+			// Create Map
+			var mapFragment = new MapFragment();
+			FragmentTransaction fragmentTx = this.FragmentManager.BeginTransaction();
+			fragmentTx.Add(Resource.Id.MapContainer, mapFragment);
+			fragmentTx.Commit();
+			GoogleMap map = mapFragment.Map;
+
+			// Set Main View
 			SetContentView (Resource.Layout.Main);
+
+			// Configure Map
+			if (map != null) {
+				this._mapHelper = new MapHelper (map);
+			}
 
 			// Tab 1
 			ActionBar.Tab tab1 = ActionBar.NewTab();
 			tab1.SetText(Resources.GetString(Resource.String.tab1));
 			//tab.SetIcon(Resource.Drawable.tab1_icon);
-			tab1.TabSelected += (sender, args) => {
-				Console.WriteLine (1);
+			tab1.TabSelected += async (sender, args) => {
+				var ok = await this._mapHelper.DisplayStations(ServiceProviderEnum.Luas);
+				if(!ok){
+					// TODO Let user know there was a connection error
+				}
 			};
 			ActionBar.AddTab(tab1);
 
@@ -34,8 +57,11 @@ namespace DublinRTPI.Android
 			ActionBar.Tab tab2 = ActionBar.NewTab();
 			tab2.SetText(Resources.GetString(Resource.String.tab2));
 			//tab.SetIcon(Resource.Drawable.tab1_icon);
-			tab2.TabSelected += (sender, args) => {
-				Console.WriteLine (2);
+			tab2.TabSelected += async (sender, args) => {
+				var ok = await this._mapHelper.DisplayStations(ServiceProviderEnum.IrishRail);
+				if(!ok){
+					// TODO Let user know there was a connection error
+				}
 			};
 			ActionBar.AddTab(tab2);
 
@@ -43,8 +69,11 @@ namespace DublinRTPI.Android
 			ActionBar.Tab tab3 = ActionBar.NewTab();
 			tab3.SetText(Resources.GetString(Resource.String.tab3));
 			//tab.SetIcon(Resource.Drawable.tab1_icon);
-			tab3.TabSelected += (sender, args) => {
-				Console.WriteLine (3);
+			tab3.TabSelected += async (sender, args) => {
+				var ok = await this._mapHelper.DisplayStations(ServiceProviderEnum.DublinBike);
+				if(!ok){
+					// TODO Let user know there was a connection error
+				}
 			};
 			ActionBar.AddTab(tab3);
 
@@ -52,8 +81,11 @@ namespace DublinRTPI.Android
 			ActionBar.Tab tab4 = ActionBar.NewTab();
 			tab4.SetText(Resources.GetString(Resource.String.tab4));
 			//tab.SetIcon(Resource.Drawable.tab1_icon);
-			tab4.TabSelected += (sender, args) => {
-				Console.WriteLine (4);
+			tab4.TabSelected += async (sender, args) => {
+				var ok = await this._mapHelper.DisplayRoutes(ServiceProviderEnum.DublinBus);
+				if(!ok){
+					// TODO Let user know there was a connection error
+				}
 			};
 			ActionBar.AddTab(tab4);
 		}
---
+++ DublinRTPI.Android/MapHelper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+using DublinRTPI.Core;
+using DublinRTPI.Core.Entities;
+using System.Threading.Tasks;
+using Android.Gms.Maps.Model;
+
+namespace DublinRTPI.Android
+{
+	public class MapHelper
+	{
+		private GoogleMap _map;
+		private DataController _dataController;
+
+		public MapHelper (GoogleMap map)
+		{
+			this._map = map;
+			this._map.MapType = GoogleMap.MapTypeNormal;
+			this._dataController = new DataController();
+		}
+
+		public void SetCamera(ServiceProviderEnum provider){
+			LatLng location = new LatLng (53.3479095, -6.2559231);
+			int zoom = 18;
+			if (provider == ServiceProviderEnum.DublinBike) {
+				location = new LatLng (53.3479095, -6.2559231);
+				zoom = 18; //10000
+			}
+			else if(provider == ServiceProviderEnum.IrishRail)
+			{
+				location = new LatLng (53.3479095, -6.2559231);
+				zoom = 18; //60000
+			}
+			else if(provider == ServiceProviderEnum.Luas)
+			{
+				zoom = 18; // 25000
+				location = new LatLng (53.3162064, -6.2672187);
+			}
+			CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+			builder.Target(location);
+			builder.Zoom(zoom);
+			CameraPosition cameraPosition = builder.Build();
+			CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
+			this._map.MoveCamera(cameraUpdate);
+		}
+
+		public void ClearMarkers(){
+			//TODO
+		}
+
+		public void AddMarker(ServiceProviderEnum provider, Station station){
+			var marker = new MarkerOptions();
+			marker.SetPosition(new LatLng(station.Latitude, station.Longitude));
+			marker.SetTitle(station.Name);
+			marker.InvokeIcon(BitmapDescriptorFactory.DefaultMarker (BitmapDescriptorFactory.HueCyan));
+			this._map.AddMarker(marker);
+		}
+
+		public async Task<bool> DisplayRoutes(ServiceProviderEnum provider){
+			var routes = await this._dataController.GetRoutes (provider);
+			if (routes.Count > 0) {
+				// TODO display route list
+				return true;
+			} 
+			else 
+			{
+				return false;
+			}
+		}
+
+		public async Task<bool> DisplayStations(ServiceProviderEnum provider){
+			this.ClearMarkers();
+			this.SetCamera(provider);
+			var stations = await this._dataController.GetStations (provider);
+			if (stations.Count > 0) 
+			{
+				foreach (var station in stations) {
+					this.AddMarker (provider, station);
+				}
+				return true;
+			} 
+			else 
+			{
+				return false;
+			}
+		}
+
+		public async Task<bool> DisplayStationsByRoute(ServiceProviderEnum provider, string route){
+			this.ClearMarkers();
+			this.SetCamera(provider);
+			var stations = await this._dataController.GetStationsByRoute (provider, route);
+			if (stations.Count > 0) 
+			{
+				foreach (var station in stations) 
+				{
+					this.AddMarker (provider, station);
+				}
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
